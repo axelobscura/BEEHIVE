fa4e3b808d7d2f177d4747717ff697396893d190
axelobscura <axosar@gmail.com>
2020-11-10 20:41:23 -0600

    nodos

--- pages/index.js
+++ pages/index.js
@@ -10,83 +10,23 @@ export default function Home() {
     <Layout>
       <Particles 
         className={styles.particulas}
-        options={{
-          background: {
-            color: {
-              value: "#0d47a1",
-            },
-          },
-          fpsLimit: 60,
-          interactivity: {
-            detectsOn: "canvas",
-            events: {
-              onClick: {
-                enable: true,
-                mode: "push",
-              },
-              onHover: {
-                enable: true,
-                mode: "repulse",
-              },
-              resize: true,
-            },
-            modes: {
-              bubble: {
-                distance: 400,
-                duration: 2,
-                opacity: 0.8,
-                size: 40,
-              },
-              push: {
-                quantity: 4,
-              },
-              repulse: {
-                distance: 200,
-                duration: 0.4,
-              },
-            },
-          },
+        params={{
           particles: {
             color: {
-              value: "#ffffff",
+              value: "#E5B131"
             },
-            links: {
-              color: "#ffffff",
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-              width: 1,
-            },
-            collisions: {
-              enable: true,
-            },
-            move: {
-              direction: "none",
-              enable: true,
-              outMode: "bounce",
-              random: false,
-              speed: 6,
-              straight: false,
+            line_linked: {
+              color: {
+                value: "#f2f2f2"
+              }
             },
             number: {
-              density: {
-                enable: true,
-                value_area: 800,
-              },
-              value: 100020,
-            },
-            opacity: {
-              value: 0.5,
-            },
-            shape: {
-              type: "circle",
+              value: 250
             },
             size: {
-              random: true,
-              value: 5,
-            },
-          },
-          detectRetina: true,
+              value: 3
+            }
+          }
         }}
       />
       <Link href='/quienes-somos'>
